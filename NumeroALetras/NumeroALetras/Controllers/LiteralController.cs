3bf31ef8693847fb6fbd311ff482643d1dcb6b4d
Dayana Nicole Cuellar Gonzales <a2018117131@estudiantes.upsa.edu.bo>
2020-08-20 15:13:44 -1200

    Este es mi proyecto

---
+++ NumeroALetras/NumeroALetras/Controllers/LiteralController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NumeroALetras.Helpers;
+
+namespace NumeroALetras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LiteralController : ControllerBase
+    {
+        [HttpGet]
+        public string literal(int numero)
+        {
+            string cadena = Conversores.NumeroALetras(numero);
+
+            return cadena;
+        }
+
+        [HttpPost]
+        public string literal1([FromHeader]int numero)
+        {
+            string cadena = Conversores.NumeroALetras(numero);
+
+            return cadena;
+        }
+    }
+}
